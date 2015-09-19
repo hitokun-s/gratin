8038df6ce4e5a5e297f96b02697e61946ccfaa07
hitokun-s <jgpuauno@gmail.com>
2015-09-19 16:57:14 +0800

    add pairs() method

--- src/util/Weight.groovy
+++ src/util/Weight.groovy
@@ -4,8 +4,13 @@ import components.Neuron
 
 /**
  * あるユニットとあるユニットをつなぐ結合の重みが１つしかない場合
- * Weight[a][b]（＝Weight[b][a]）
- * の形で重みにアクセスできる
+ * Weight[a,b]（＝Weight[b,a]）の形で重みにアクセスできる
+ *
+ * [a,b]というListをキーにしたことで、重みだけでなく、pairs()によって結合全体を取得することもできる
+ * （Weightという名前の趣旨からするとちょっと変だけど）
+ *
+ * 相互結合ネットワークの場合は、自己結合も重み0で作成するようにした
+ * その方が、あるニューロン周りの結合を巡回して計算する実装がシンプルになるため。
  */
 class Weight extends HashMap<List<Neuron>, Double>{
 
@@ -15,7 +20,9 @@ class Weight extends HashMap<List<Neuron>, Double>{
      */
     public Weight(List<Neuron> list){
         [list,list].eachCombination { List pair ->
-            if(pair[0] == pair[1])return // 自己結合は除外
+            if(pair[0] == pair[1]){
+                super.put(pair, 0) // 自己結合の重みは0とする
+            }
             if(super.get(pair) || super.get(pair.reverse())) return // 登録済ならスキップ
             // superを使わないと、Overrideしている自身のget,put内の存在確認assertionで引っかかってしまう
             super.put(pair, Math.random() )// 重みは乱数で初期化
@@ -50,5 +57,13 @@ class Weight extends HashMap<List<Neuron>, Double>{
         }
         assert super.get(key)
         super.get(key)
+    }
+
+    /**
+     * 本当はメソッド名：getPairs()にして、外部からは、Weight.pairs でアクセスできれば良かったが、
+     * 本クラスはHashMapなので、Weight.pairsとした瞬間に、Weight.get("pairs")を呼びにいってしまうので、あきらめた
+     */
+    public List<List<Neuron>> pairs(){
+        this.keySet() as List
     }
 }
