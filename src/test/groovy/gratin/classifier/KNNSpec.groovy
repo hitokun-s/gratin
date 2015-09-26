d62f10b07c300596211c237469b53c317b8d2a8c
hitokun-s <jgpuauno@gmail.com>
2015-09-26 09:12:23 +0800

    add test for KNN in case of classify based on majority of multiple neighbors

--- src/test/groovy/gratin/classifier/KNNSpec.groovy
+++ src/test/groovy/gratin/classifier/KNNSpec.groovy
@@ -8,7 +8,7 @@ import spock.lang.Specification
  */
 class KNNSpec extends Specification {
 
-    def "classify"() {
+    def "classify based on one nearest neighbor"() {
         given:
             def knn = new KNN([
                 [input: [2, -5, 7], output: [1, 0, 0]], // dist square = 1 + 49 + 16 = 66
@@ -16,8 +16,23 @@ class KNNSpec extends Specification {
                 [input: [8, 4, -5], output: [0, 0, 1]]// dist square = 49 + 4 + 64 = 117
             ])
         when:
-            def res = knn.classify([1,2,3])
+            def res = knn.classify([1, 2, 3])
         then:
-            res == [0,1,0]
+            res == [0, 1, 0]
+    }
+
+    def "classify based on multiple neighbors"() {
+        given:
+            def knn = new KNN([
+                [input: [2, -5, 7], output: [1, 0, 0, 0]], // dist square = 1 + 49 + 16 = 66
+                [input: [-3, 1, 6], output: [0, 1, 0, 0]], // dist square = 16 + 1 + 9 = 26
+                [input: [-3, 4, 6], output: [0, 0, 0, 1]], // dist square = 16 + 4 + 9 = 29
+                [input: [-3, 1, 7], output: [0, 0, 0, 1]], // dist square = 16 + 1 + 16 = 33
+                [input: [8, 4, -5], output: [0, 0, 1, 0]]  // dist square = 49 + 4 + 64 = 117
+            ])
+        when:
+            def res = knn.classify([1, 2, 3], 3) // decided by majority of top 3 neighbors
+        then:
+            res == [0, 0, 0,1] // not the most nearest pattern [0,1,0,0]
     }
 }
