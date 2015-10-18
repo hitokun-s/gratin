c58f6f1bfcb7e96d3c351177eed09ed09c74aef6
hitokun-s <jgpuauno@gmail.com>
2015-10-18 21:39:31 +0700

    add pooling layer

---
+++ src/main/groovy/gratin/layers/PoolingLayer.groovy
@@ -0,0 +1,79 @@
+package gratin.layers
+
+import gratin.components.Neuron
+import gratin.image.Filter
+import gratin.util.Bond
+import gratin.util.Matrix
+import gratin.util.Matrix3D
+import gratin.util.NMatrix3D
+
+/**
+ * Pooling Layer
+ * @author Hitoshi Wada
+ */
+class PoolingLayer {
+
+    NMatrix3D inputs
+    NMatrix3D outputs
+    int windowSize
+    int stride
+    int channelSize
+
+    PoolingLayer(NMatrix3D inputs, NMatrix3D outputs, Map opt = []) {
+
+        assert inputs.depth == outputs.depth // 入力チャネル毎にプーリング処理をして出力するので、入力チャネル数＝出力チャネル数、になる。
+
+        this.inputs = inputs
+        this.outputs = outputs
+        channelSize = inputs.depth
+
+        windowSize = opt.windowSize ?: 5
+        stride = opt.stride ?: 4
+        assert (int) ((inputs.row - 1) / stride) + 1 == outputs.row // stride > 1なら出力サイズは縮小する
+
+        createBond()
+    }
+
+    def createBond() {
+        // TODO プーリング層の場合は、最大プーリングでも平均プーリングでも、プーリング窓内の画素の扱いに位置依存性がないので、
+        // 本当はわざわざBondを作成しなくても良い気がする。。。
+        channelSize.times { int cIdx -> // channel index
+            inputs[cIdx].forEachWithIndexByStride(stride) { Neuron v, int center_i, int center_j, int strideX, int strideY ->
+                def radius = (windowSize - 1) / 2
+                int i = center_i - radius, j = center_j - radius
+                for (int m = 0; m < windowSize; m++) {
+                    if (i + m < 0 || i + m >= inputs[cIdx].rowCount) continue // はみ出る領域については結合なしと定義する
+                    for (int n = 0; n < windowSize; n++) {
+                        if (j + n < 0 || j + n >= inputs[cIdx].colCount) continue // はみ出る領域については結合なしと定義する
+                        new Bond(inputs[cIdx][i + m][j + n], outputs[cIdx][strideX][strideY])
+                    }
+                }
+            }
+        }
+    }
+
+    def forward() {
+        // max pooling （最大プーリング）
+        outputs.forEach { Neuron outN ->
+            def maxBond = Bond.findAllByE(outN).max { Bond b ->
+                b.w = 0 // 仮重みを初期化
+                b.s.value
+            }
+            maxBond.w = 1 // backwardのための目印
+            outN.value = maxBond.s.value
+        }
+    }
+
+    def backward() {
+        inputs.forEach { Neuron inN -> inN.delta = 0 } // デルタを初期化
+
+        // 最大プーリング方式でのbackwardでは、forwardで最大として選ばれた入力ユニット（だけ）に、
+        // 出力ユニットのδがそのまま逆伝播する
+        // よって計算効率化のために、仮重み（0/1）を使って、forwardにおいて、
+        // 最大プーリングで選択されたBondのみw=1にし、他はw=0にしておく。すっきり！
+        outputs.forEach { Neuron outN ->
+            def maxBond = Bond.findAllByE(outN).find { it.w == 1 }
+            maxBond.s.delta += outN.delta // maxBond.s.delta = outN.delta だと、重複してmaxになる場合にマズイ気がする。
+        }
+    }
+}
---
+++ src/test/groovy/gratin/layers/PoolingLayerSpec.groovy
@@ -0,0 +1,69 @@
+package gratin.layers
+
+import gratin.components.Neuron
+import gratin.image.ImageUtil
+import gratin.util.*
+import spock.lang.Specification
+
+import javax.imageio.ImageIO
+import java.awt.*
+import java.awt.image.BufferedImage
+import java.util.List
+
+
+/**
+ * @author Hitoshi Wada
+ */
+class PoolingLayerSpec extends Specification {
+
+    def "constructor"() {
+        given:
+            def (n1, n2, n3, n4, n5, n6, n7, n8, n9, n10,
+                 n11, n12, n13, n14, n15, n16, n17, n18, n19, n20,
+                 n21, n22, n23, n24, n25, n26, n27, n28, n29, n30,
+                 n31, n32) = (1..32).collect { new Neuron(idx: it) }
+            // 入力は２チャネル
+            def inputs = new NMatrix3D([
+                new NMatrix([
+                    [n1, n2, n3, n4],
+                    [n5, n6, n7, n8],
+                    [n9, n10, n11, n12]
+                ]),
+                new NMatrix([
+                    [n13, n14, n15, n16],
+                    [n17, n18, n19, n20],
+                    [n21, n22, n23, n24]
+                ])
+            ])
+            // 出力も２チャネル（でなければならない！）
+            def outputs = new NMatrix3D([
+                new NMatrix([
+                    [n25, n26],
+                    [n27, n28]
+                ]),
+                new NMatrix([
+                    [n29, n30],
+                    [n31, n32]
+                ])
+            ])
+        when:
+            def layer = new PoolingLayer(inputs, outputs, [
+                windowSize: 3,
+                stride    : 2
+            ])
+            Bond.findAllByE(n28).each{
+                println it
+            }
+        then:
+            Bond.findAllByE(n28).size() == 6
+            Bond.findAllByE(n28).containsAll([
+                Bond.findBySandE(n6, n28),
+                Bond.findBySandE(n7, n28),
+                Bond.findBySandE(n8, n28),
+                Bond.findBySandE(n10, n28),
+                Bond.findBySandE(n11, n28),
+                Bond.findBySandE(n12, n28)
+            ])
+    }
+
+}
